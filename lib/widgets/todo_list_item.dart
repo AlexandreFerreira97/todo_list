9d7a4561cad98a1f26969515abed145a1c529569
Alexandre <alexandrefaria3012@gmail.com>
2023-06-12 16:29:42 -0300

    update

--- lib/widgets/todo_list_item.dart
+++ lib/widgets/todo_list_item.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 
-class TodoListItem extends StatelessWidget {
-  const TodoListItem({Key? key, required this.title}) : super(key: key);
+import '../models/todo.dart';
 
-  final String title;
+class TodoListItem extends StatelessWidget {
+  const TodoListItem({Key? key, required this.todo}) : super(key: key);
+
+  final Todo todo;
 
   @override
   Widget build(BuildContext context) {
@@ -17,12 +19,12 @@ class TodoListItem extends StatelessWidget {
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          const Text(
-            '22/05/2012',
-            style: TextStyle(fontSize: 12),
+          Text(
+            todo.dateTime.toString(),
+            style: const TextStyle(fontSize: 12),
           ),
           Text(
-            title,
+            todo.title,
             style: const TextStyle(
               fontSize: 16,
               fontWeight: FontWeight.w600,
