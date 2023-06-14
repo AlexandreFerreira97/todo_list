c94c83941b0c0480aff825b5bd6aa72be3baf648
Alexandre <alexandrefaria3012@gmail.com>
2023-06-14 19:33:12 -0300

    Update todo_list_page.dart

--- lib/pages/todo_list_page.dart
+++ lib/pages/todo_list_page.dart
@@ -123,6 +123,7 @@ class _TodoListPageState extends State<TodoListPage> {
     setState(() {
       todos.remove(todo);
     });
+    todoRepository.saveTodoList(todos);
 
     ScaffoldMessenger.of(context).clearSnackBars();
     ScaffoldMessenger.of(context).showSnackBar(SnackBar(
@@ -140,6 +141,7 @@ class _TodoListPageState extends State<TodoListPage> {
           setState(() {
             todos.insert(deletedTodoPos!, deletedTodo!);
           });
+          todoRepository.saveTodoList(todos);
         },
       ),
       duration: const Duration(seconds: 5),
@@ -177,5 +179,6 @@ class _TodoListPageState extends State<TodoListPage> {
     setState(() {
       todos.clear();
     });
+    todoRepository.saveTodoList(todos);
   }
 }
